666a63b1f400bf556ffc26d62f4c16469e7cfb7c
Mohit <dhanwaniap@gmail.com>
2026-07-12 15:46:15 +0530

    fix: bugs

--- backend/src/middlewares/rateLimiter.middleware.ts
+++ backend/src/middlewares/rateLimiter.middleware.ts
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit';
 
 export const generalRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  max: 1000, // limit each IP to 100 requests per windowMs
   message: { success: false, message: 'Too many requests, please try again later.' },
 });
 
